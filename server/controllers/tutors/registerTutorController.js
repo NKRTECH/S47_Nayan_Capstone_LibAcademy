22e4fa44ad468127685b9c9202ee572f28e5ad9a
NKRTECH <141981269+NKRTECH@users.noreply.github.com>
2024-03-07 11:59:41 +0530

    checking existing email and saving hashed password of learners to database

--- server/controllers/tutors/registerTutorController.js
+++ server/controllers/tutors/registerTutorController.js
@@ -4,7 +4,15 @@ const Tutors = require('../../models/tutors/tutors');
 
 const registerTutorController = async (req, res) => {
     try {
-        // Generate a salt and hash the password using bcrypt
+        const { email } = req.body;
+
+        // Check if the tutor already exists
+        const existingTutor = await Tutors.findOne({ email });
+        if (existingTutor) {
+            return res.status(400).json({ message: 'Email is already registered' });
+        }
+
+        // Hash the password using bcrypt
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
         // Create a new tutor document with the hashed password
@@ -15,9 +23,13 @@ const registerTutorController = async (req, res) => {
         
         // Omit the password field from the response for security reasons
         const { password, ...tutorDataWithoutPassword } = tutor.toObject();
+
         res.status(201).send(tutorDataWithoutPassword);
-        } catch (error) {
-        res.status(400).send(error);
+    } catch (error) {
+        // Handle errors
+        console.error('Error registering tutor:', error);
+        res.status(500).send({ message: 'Internal server error' });
     }
 };
+
 module.exports = registerTutorController;
